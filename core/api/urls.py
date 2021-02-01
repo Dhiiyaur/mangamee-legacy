9ae65a93cc369fc4a0749ee7a354868aba1fb54f
Dhiiyaur <dhiyarifqi@gmail.com>
2021-02-01 22:59:24 +0700

    update user login, register, history api

--- core/api/urls.py
+++ core/api/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('search/', views.searchMangaAPI.as_view(), name= 'searchMangaAPI'),
     path('manga/', views.chapterMangaAPI.as_view(), name= 'chapterMangaAPI'),
     path('page/', views.pageMangaAPI.as_view(), name= 'PageMangaAPI'),
+    path('auth/history/', views.userGetHistory.as_view(), name= 'userGetHistory'),
+    path('auth/login/', views.userLoginAPI.as_view(), name= 'userLoginAPI'),
+    path('auth/register/', views.userRegisterAPI.as_view(), name= 'userRegisterAPI'),
 
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -12,6 +12,125 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+import json
+import pyrebase
+from core.config import *
+from rest_framework.parsers import JSONParser 
+
+# user auth  -----------------------------------------------
+
+
+firebaseConfig = {
+
+    "apiKey"        : apiKey_firebase,
+    "authDomain"    : authDomain_firebase,
+    "databaseURL"   : databaseUR_firebase ,
+    "storageBucket" : storageBucket_firebase
+
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth    = firebase.auth()
+db      = firebase.database()
+
+# --------------------------------------------------------------
+
+
+class userLoginAPI(APIView):
+
+    def post(self, request):
+
+        data = request.data
+        
+        # --------------- data params 
+
+        userEmail = data['email']
+        userPassword = data['password']
+
+        #----------------------------------
+
+        try:
+            user = auth.sign_in_with_email_and_password(userEmail, userPassword)
+        except:
+            message = {
+                'message' : 'account doesnt exist'
+            }
+            respone = Response(message, status=status.HTTP_404_NOT_FOUND)
+            return respone
+
+        token = user['localId']
+        userData = {
+            'token' : token,
+
+        }
+        respone = Response(userData, status=status.HTTP_200_OK)
+        return respone
+
+
+class userRegisterAPI(APIView):
+
+    def post(self, request):
+
+        data = request.data
+
+        # --------------- data params 
+        userName = data['username']
+        userEmail = data['email']
+        userPassword = data['password']
+
+        #----------------------------------
+        try:
+            user = auth.create_user_with_email_and_password(userEmail, userPassword)
+
+        except:
+            message = {
+                'message' : 'email already exist'
+            }
+            respone = Response(message, status=status.HTTP_404_NOT_FOUND)
+            return respone
+        
+        uid  = user['localId']
+        data = {
+
+            'name' : userName
+        }
+
+        db.child('users').child(uid).set(data)
+
+        message = {
+            'message' : 'OK'
+        }
+        respone = Response(message, status=status.HTTP_200_OK)
+        return respone
+
+
+
+
+class userGetHistory(APIView):
+
+    def post(self, request):
+
+        data = request.data
+
+        # --------------- data params 
+
+        userToken = data['token']
+
+        #----------------------------------
+
+        userName = db.child("users").get().val().get(userToken).get('name')
+        userHistory = db.child("userHistory").get().val().get(userName)
+
+        userData = {
+
+            'history' : userHistory
+        }
+
+        respone = Response(userData, status=status.HTTP_200_OK)
+        return respone
+
+
+
 
 class browseMangaAPI(APIView):
 
@@ -26,8 +145,12 @@ class searchMangaAPI(APIView):
 
     def get(self, request):
 
+        # --------------- data params 
+
         title = request.GET.get('manga_title')
         lang_select = request.GET.get('lang')
+
+        # ------------------------------
 
         if lang_select == 'EN':
             result = MH_manga_name(title)
@@ -42,8 +165,12 @@ class chapterMangaAPI(APIView):
 
     def get(self, request):
 
+        # --------------- data params 
+
         title = request.GET.get('manga_title')
         lang_select = request.GET.get('lang')
+
+        # ------------------------------
 
         if lang_select == 'EN':
 
@@ -76,9 +203,13 @@ class pageMangaAPI(APIView):
 
     def get(self, request):
 
+        # --------------- data params 
+
         title = request.GET.get('manga_title')
         chapter = request.GET.get('chapter')
         lang_select = request.GET.get('lang')
+
+        # ------------------------------
 
         if lang_select == 'EN':
             result = MH_manga_image(title, chapter)
